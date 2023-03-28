1f01777662d19c713887715be23e4aa52612c42d
Richard Washington <richard.washington@taxassist.co.uk>
2023-03-28 22:51:36 +0100

    :art: Set dark and light theme and enable toggling between the two

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 
+import { useState } from 'react';
+
+
 function App() {
+  const [mode, setMode] = useState('dark');
+
+  function toggleMode() {
+    setMode(mode === 'dark' ? '' : 'dark');
+  }
+
   return (
-    <div>
-      <p className="text-3xl font-bold underline">Hello</p>
+    <div className={mode}>
+      <div className="bg-slate-100 dark:bg-slate-900 w-full h-screen flex">
+        <button className="m-auto rounded p-2 dark:text-slate-900 dark:bg-slate-100 text-slate-100 bg-slate-900" onClick={toggleMode}>Go {mode === 'dark' ? 'light' : 'dark'}</button>
+      </div>
     </div>
   );
 }
