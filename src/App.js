ebd517453f1f660e7ffbe48c9f61ccebfb37d7ad
Richard Washington <richard.washington@taxassist.co.uk>
2023-03-29 09:14:33 +0100

    :memo:  Remove junk text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import { useState } from 'react';
+import Form from './Form.js'
+import Cv from './Cv.js'
 
 
 function App() {
@@ -11,8 +13,10 @@ function App() {
 
   return (
     <div className={mode}>
-      <div className="bg-slate-100 dark:bg-slate-900 w-full h-screen flex">
-        <button className="m-auto rounded p-2 dark:text-slate-900 dark:bg-slate-100 text-slate-100 bg-slate-900" onClick={toggleMode}>Go {mode === 'dark' ? 'light' : 'dark'}</button>
+      <div className="bg-slate-100 dark:bg-slate-900 w-full h-screen flex flex-col items-center">
+        <div className="m-2"><button className="rounded p-2 dark:text-slate-900 dark:bg-slate-100 text-slate-100 bg-slate-900" onClick={toggleMode}>Go {mode === 'dark' ? 'light' : 'dark'}</button></div>
+        <div className="m-2"><Form /></div>
+        <div className="m-2"><Cv /></div>
       </div>
     </div>
   );
