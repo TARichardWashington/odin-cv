ed663f91a117264cfba9f372c34d50a86a37903f
Richard Washington <richard.washington@taxassist.co.uk>
2023-04-01 18:41:26 +0100

    :art: Set dark and light background styles

--- src/App.js
+++ src/App.js
@@ -18,12 +18,12 @@ function App() {
 
   return (
     <div className={'flex flex-col ' + mode}>
-      <div className="p-2 bg-slate-700">
-        <button className="rounded p-2 dark:text-slate-900 dark:bg-slate-100 text-slate-100 bg-slate-900" onClick={toggleMode}>Go {mode === 'dark' ? 'light' : 'dark'}</button>
+      <div className="p-2 dark:bg-slate-900 bg-slate-300">
+        <button className="rounded p-2 dark:text-slate-900 dark:bg-slate-300 text-slate-100 bg-slate-900" onClick={toggleMode}>{mode === 'dark' ? 'Light' : 'Dark'} mode</button>
       </div>
-      <div className="p-2 bg-slate-100 flex-1 flex justify-center">
-        <div className="p-5 bg-slate-900 rounded text-slate-100">
-          <h1 className="text-xl">CV</h1>
+      <div className="p-2 dark:bg-slate-800 bg-slate-200 flex-1 flex justify-center">
+        <div className="p-5 dark:bg-slate-900 bg-slate-300 rounded dark:text-slate-100 text-slate-900">
+          <h1 className="text-xl text-center pb-5">CV</h1>
           <form>
             <label className="pr-5">Name</label>
             <input type="text" className="text-slate-900 rounded p-1" onChange={setCVName} />
